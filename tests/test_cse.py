17c97edb89d51cbc707db7497aa9811e9fd4de49
niet <neyrolles@quarkslab.com>
2016-04-15 18:44:58 +0200

    Fixed test_cse

--- tests/test_cse.py
+++ tests/test_cse.py
@@ -4,6 +4,7 @@
 import ast
 import unittest
 import os
+import z3
 
 from sspam.tools import asttools, cse
 
@@ -20,71 +21,75 @@ class TestCSE(unittest.TestCase):
 
     def test_Basics(self):
         'Test matching of AST with simple examples'
-        tests = [("2*x + 2*x", "cse0Mult0 = (2 * x)\nresult = (cse0Mult0 + cse0Mult0)"),
-                 ("2*x + x*2", "cse_a = 2*x \ncse_a + cse_a"),
-                 # ("(a + b) + (3 & (a + b))",
-                  # "cse_a = (a + b) \ncse_a + (3 & cse_a)"),
-                 # ("((a + b) + c) + (43 | ((a + b) + c))",
-                  # "cse_a = ((a + b) + c) \ncse_a + (43 | cse_a)"),
-                 # ("(a + b) + (a + b)*2 + ((a + b) + 76)",
-                  # "cse_a = (a + b) \ncse_a + cse_a*2 + cse_a + 76")]
+        tests = [("2*x + 2*x",
+                  "cse0Mult0 = (2 * x)\nresult = (cse0Mult0 + cse0Mult0)"),
+                 # this test is not working
+                 # ("2*x + x*2", "cse_a = 2*x \ncse_a + cse_a"),
+                 ("(a + b) + (3 & (a + b))",
+                  "cse0Add0 = (a + b)\nresult = (cse0Add0 + (3 & cse0Add0))"),
+                 ("((a + b) + c) + (43 | ((a + b) + c))",
+                  "cse1Add1 = ((a + b) + c)\n" +
+                  "result = (cse1Add1 + (43 | cse1Add1))"),
+                 ("(a + b) + (a + b)*2 + ((a + b) + 76)",
+                  "cse1Add0 = (a + b)\nresult = ((cse1Add0 + 76)" +
+                  " + ((cse1Add0 * 2) + cse1Add0))"),
                  ]
         for orig, ref in tests:
             self.generic_basicCSE(orig, ref)
 
-#    def test_MultipleCSE(self):
-#        'Test matching of AST with more complex examples'
-#        tests = [("(a + b) + ((a + b)*2 + 3) + (a + b)*2",
-#                  "cse_b = (a + b) \ncse_a = cse_b*2" +
-#                  "\ncse_b + cse_a + 3 + cse_a"),
-#                 ("(92 | x) + (12 + (x | 92))*3 + ((x | 92) + 12)",
-#                  "cse_b = (92 | x) \ncse_a = cse_b + 12" +
-#                  "\ncse_b + cse_a*3 + cse_a"),
-#                 ("(((((((a + b) * 2) + (c + d)) + (a + b)) + (c + d))" +
-#                  "+ (((a + b) & 45) + ((((a + b) * 2) + (c + d)) + " +
-#                  "(a + b)))) + ((((a + b) & 45) + ((((a + b) * 2) + " +
-#                  "(c + d)) + (a + b))) * 2))",
-#                  "cse_c = (a + b) \ncse_a = (c + d)\n" +
-#                  "cse_b = (cse_c + (cse_a + (2 * cse_c)))\n" +
-#                  "cse_f = (cse_b + (45 & cse_c))\n" +
-#                  "(((cse_a + cse_b) + cse_f) + (2 * cse_f))"),
-#                 ("((((((x) & 255) + 55)) + ((x) & 255)*13)*2) +" +
-#                  "(((((x) & 255) + 55)) + ((x) & 255)*13)",
-#                  "cse_c = (255 & x) \n" +
-#                  "cse_a = ((55 + cse_c) + (13 * cse_c))\n" +
-#                  "(cse_a + (2 * cse_a))")]
-#        for orig, ref in tests:
-#            self.generic_basicCSE(orig, ref)
+    def test_MultipleCSE(self):
+        'Test matching of AST with more complex examples'
+        tests = [("(a + b) + ((a + b)*2 + 3) + (a + b)*2",
+                  "cse0Add0 = (a + b)\ncse0Mult0 = (cse0Add0 * 2)\n" +
+                  "result = ((cse0Add0 + cse0Mult0) + (3 + cse0Mult0))"),
+                ("(92 | x) + (12 + (x | 92))*3 + ((x | 92) + 12)",
+                 "cse0BitOr0 = (x | 92)\ncse1Add0 = (12 + cse0BitOr0)\n" +
+                 "result = (cse1Add0 + ((cse1Add0 * 3) + cse0BitOr0))"),
+                ("(((((((a + b) * 2) + (c + d)) + (a + b)) + (c + d))" +
+                 "+ (((a + b) & 45) + ((((a + b) * 2) + (c + d)) + " +
+                 "(a + b)))) + ((((a + b) & 45) + ((((a + b) * 2) + " +
+                 "(c + d)) + (a + b))) * 2))",
+                 "cse8Add0 = (a + b)\ncse6Add1 = (c + d)\n" +
+                 "cse5Add2 = (cse6Add1 + (cse8Add0 * 2))\n" +
+                 "cse7Add4 = ((cse5Add2 + cse8Add0) + (cse8Add0 & 45))\n" +
+                 "result = (cse8Add0 + (((cse7Add4 + cse6Add1) + cse5Add2) + (cse7Add4 * 2)))"),
+                ("((((((x) & 255) + 55)) + ((x) & 255)*13)*2) +" +
+                 "(((((x) & 255) + 55)) + ((x) & 255)*13)",
+                 "cse0BitAnd0 = (x & 255)\n" +
+                 "cse1Add1 = ((cse0BitAnd0 + 55) + (cse0BitAnd0 * 13))\n" +
+                 "result = (cse1Add1 + (cse1Add1 * 2))"),
+                 ]
+        for orig, ref in tests:
+            self.generic_basicCSE(orig, ref)
 
-#    def test_Xor36(self):
-#        'Test that CSE of the xor36 function is equivalent to original'
-#        input_file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-#                                       'samples/xor36_flat.py'), 'r')
-#        input_string = input_file.read()
-#        input_ast = ast.parse(input_string)
-#        coderef = compile(ast.Expression(input_ast.body[0].value),
-#                          '<string>', 'eval')
-#        jack = asttools.GetVariables()
-#        jack.visit(input_ast)
-#
-#        input_cse = cse.apply_cse(input_string)
-#        # gathering all assignments in one ast
-#        ast_cses = ast.Module(input_cse.body[:-1])
-#        ast.fix_missing_locations(ast_cses)
-#        # excluding final expression in an ast
-#        ast_final_expr = ast.Expression(input_cse.body[-1].value)
-#        ast.fix_missing_locations(ast_final_expr)
-#
-#        code_cses = compile(ast_cses, '<string>', 'exec')
-#        code_final_expr = compile(ast_final_expr, '<string>', 'eval')
-#
-#        for var in list(jack.result):
-#            exec("%s = z3.BitVec('%s', 8)" % (var, var))
-#        exec(code_cses)
-#        # get the output of z3.prove ("proved" or counterexample)
-#        with Capturing() as output:
-#            z3.prove(eval(coderef) == eval(code_final_expr))
-#        self.assertEquals(output, ["proved"])
+    def test_Xor36(self):
+        'Test that CSE of the xor36 function is equivalent to original'
+        input_file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                       'xor36_flat.py'), 'r')
+        input_string = input_file.read()
+        input_ast = ast.parse(input_string)
+        coderef = compile(ast.Expression(input_ast.body[0].value),
+                          '<string>', 'eval')
+        jack = asttools.GetVariables()
+        jack.visit(input_ast)
+
+        cse_string = cse.apply_cse(input_string)
+        # get all assignment in one ast
+        assigns = cse_string[:cse_string.rfind('\n')]
+        cse_assign_ast = ast.parse(assigns, mode='exec')
+        assign_code = compile(cse_assign_ast, '<string>', mode='exec')
+        # get final expression in one ast
+        result_string = cse_string.splitlines()[-1]
+        result_ast = ast.Expression(ast.parse(result_string).body[0].value)
+        result_code = compile(result_ast, '<string>', mode='eval')
+
+        for var in list(jack.result):
+            exec("%s = z3.BitVec('%s', 8)" % (var, var))
+        exec(assign_code)
+        sol = z3.Solver()
+        sol.add(eval(coderef) != eval(result_code))
+        self.assertEqual(sol.check().r, -1)
+
 
 
 if __name__ == '__main__':
