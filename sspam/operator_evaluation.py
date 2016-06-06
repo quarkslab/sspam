7087a40b3489d73c668dd270f90823af4f8d4691
niet <neyrolles@quarkslab.com>
2016-06-06 15:10:43 +0200

    Added first scripts for bvsize computation and operator evalutation

---
+++ sspam/operator_evaluation.py
@@ -0,0 +1,63 @@
+"""Module to evaluate different operators not existing in Python.
+For now, operators will only be evaluated on constant expressions.
+
+Supported operators are:
+- sspam_rol
+- sspam_ror
+
+"""
+
+import ast
+import astunparse
+
+from sspam.tools import asttools
+
+
+def sspam_rol(val, rbits, maxbits):
+    upper = (val << rbits) & (2**maxbits - 1)
+    lower = (val >> (maxbits - (rbits % maxbits)))
+    return upper | lower
+
+
+def sspam_ror(val, rbits, maxbits):
+    lower = (val >> rbits % maxbits)
+    upper = (val << (maxbits - (rbits % maxbits)) & (2**maxbits - 1))
+    return upper | lower
+
+KNOWN_OPERATORS = [sspam_rol, sspam_ror]
+
+
+class EvaluateOperators(ast.NodeTransformer):
+    """
+    If operator is in KNOWN_OPERATORS and all arguments are constant
+    expressions, evaluates the operator.
+    """
+
+    def visit_Call(self, node):
+        'Check operator name and if arguments are constant'
+
+        if not node.func.id in [f.func_name for f in KNOWN_OPERATORS]:
+            return self.generic_visit(node)
+        for arg in node.args:
+            if not asttools.CheckConstExpr().visit(arg):
+                return self.generic_visit(node)
+        # working for sspam_rol, sspam_ror
+        firstop = node.args[0]
+        secop = node.args[1]
+        maxbits = firstop.nbits
+        if not isinstance(firstop, ast.Num):
+            firstop = asttools.ConstFolding(firstop,
+                                            firstop.nbits).visit(firstop)
+        if not isinstance(secop, ast.Num):
+            secop = asttools.ConstFolding(secop, secop.nbits).visit(secop)
+        rbits = secop.n
+        val = firstop.n
+        res = eval("%s(val, rbits, maxbits)" % node.func.id)
+        return ast.Num(res)
+
+
+a = ast.parse('sspam_rol(33 + 1, 3)')
+setattr(a.body[0].value.args[0], 'nbits', 8)
+a = EvaluateOperators().visit(a)
+print ast.dump(a)
+print astunparse.unparse(a)
