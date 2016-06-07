fb40346f236391b7d1f6593c85eb68690cf443ee
niet <neyrolles@quarkslab.com>
2016-06-07 14:25:31 +0200

    Added bvsize reduction

--- sspam/tools/compute_bvsize.py
+++ sspam/tools/compute_bvsize.py
@@ -7,7 +7,7 @@ import re
 
 from sspam import simplifier
 from sspam import arithm_simpl
-
+from sspam.tools import asttools
 
 class SizeError(Exception):
     pass
@@ -27,7 +27,12 @@ class ComputeBvSize(ast.NodeTransformer):
 
     def visit_Module(self, node):
         'Module stores the maxnbits'
-        setattr(node, 'nbits', self.maxnbits)
+        setattr(node, 'bvsize', self.maxnbits)
+        return self.generic_visit(node)
+
+    def visit_Expr(self, node):
+        'Expr also on maxnbits'
+        setattr(node, 'bvsize', self.maxnbits)
         return self.generic_visit(node)
 
     def visit_Call(self, node):
@@ -36,18 +41,18 @@ class ComputeBvSize(ast.NodeTransformer):
         match = re.search('bv([0-9]+)$', node.func.id)
         if match:
             if len(node.args) != 1:
-                setattr(node, 'nbits', self.maxnbits)
+                setattr(node, 'bvsize', self.maxnbits)
                 return self.generic_visit(node.args)
-            nbits = int(match.group(1))
-            setattr(node.args[0], 'nbits', nbits)
+            bvsize = int(match.group(1))
+            setattr(node.args[0], 'bvsize', bvsize)
             self.generic_visit(node.args[0])
             return node.args[0]
         # recognized functions
         if node.func.id in {"sspam_rol", "sspam_ror"}:
             self.generic_visit(node)
-            setattr(node, 'nbits', getattr(node.args[0], 'nbits'))
+            setattr(node, 'bvsize', getattr(node.args[0], 'bvsize'))
             return node
-        setattr(node, 'nbits', self.maxnbits)
+        setattr(node, 'bvsize', self.maxbnbits)
         return self.generic_visit(node)
 
     def visit_BinOp(self, node):
@@ -55,28 +60,95 @@ class ComputeBvSize(ast.NodeTransformer):
 
         self.generic_visit(node)
         try:
-            lsize = getattr(node.left, 'nbits')
-            rsize = getattr(node.right, 'nbits')
+            lsize = getattr(node.left, 'bvsize')
+            rsize = getattr(node.right, 'bvsize')
         except AttributeError:
             raise SizeError("Size missing at node %s" % compact_repr(node))
-        if lsize != rsize:
-            raise SizeError("Arguments of different sizes at node %s"
-                            % compact_repr(node))
-        setattr(node, 'nbits', max(lsize, rsize))
+#        if lsize != rsize:
+#            raise SizeError("Arguments of different sizes at node %s"
+#                            % compact_repr(node))
+        setattr(node, 'bvsize', max(lsize, rsize))
         return node
 
     def visit_UnaryOp(self, node):
         'The size of a unary op is the size of its operand'
 
         self.generic_visit(node)
-        setattr(node, 'nbits', getattr(node.operand, 'nbits'))
+        setattr(node, 'bvsize', getattr(node.operand, 'bvsize'))
         return node
 
     def visit_Name(self, node):
-        'Identifiers have maxnbits for a start'
+        'Identifiers have maxbnbits for a start'
 
-        setattr(node, 'nbits', self.maxnbits)
+        setattr(node, 'bvsize', self.maxnbits)
         return node
+
+
+class ReduceBvSize(ast.NodeTransformer):
+    """
+    If operands of an And node have different bit-vector sizes, the node
+    with the bigger size can be reduced if it contains no function and
+    no right shift.
+    """
+
+    def __init__(self):
+        self.reducing = None
+
+    def visit_BinOp(self, node):
+        'Check if node is an And and if operand can be reduced'
+
+        # version without computation of the "real" size of constant
+        # nodes
+        if not self.reducing:
+            if isinstance(node.op, ast.BitAnd):
+                lsize = getattr(node.left, 'bvsize')
+                rsize = getattr(node.right, 'bvsize')
+                if lsize < rsize:
+                    self.reducing = lsize
+                    self.visit(node.right)
+                elif rsize < lsize:
+                    self.reducing = rsize
+                    self.visit(node.left)
+            return self.generic_visit(node)
+        # reducing
+        else:
+            if isinstance(node.op, ast.RShift):
+                return node
+            setattr(node, 'bvsize', self.reducing)
+            return self.generic_visit(node)
+
+    def visit_UnaryOp(self, node):
+        'Reduce node if reduction in progress'
+        if self.reducing:
+            setattr(node, 'bvsize', self.reducing)
+        return self.generic_visit(node)
+
+    def visit_Num(self, node):
+        'Reduce node if reduction in progress'
+        if self.reducing:
+            setattr(node, 'bvsize', self.reducing)
+        return node
+
+    def visit_Call(self, node):
+        'Interrupting reduction if encountering a call node'
+
+        if self.reducing:
+            return node
+        else:
+            return self.generic_visit(node)
+
+
+class DisplayBvSize(ast.NodeVisitor):
+
+    def visit_BinOp(self, node):
+        print type(node.left), node.op, type(node.right), ': ', getattr(node, 'bvsize')
+        self.generic_visit(node)
+
+    def visit_UnaryOp(self, node):
+        print node.op, type(node.operand), ': ', getattr(node, 'bvsize')
+
+    def visit_Num(self, node):
+        print node.n, ': ', getattr(node, 'bvsize')
 
 
 expr = """(bv64(0x66FFFFFFF93D8D48) + sign_extend(~(byte_swap(((((ror((Mem32(Addr64((Addr64((Mem64((((((((((((((((((((((Sym0 - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) * bv64(0x00000001)) + bv64(0x00000090))) + bv64(0x00000000))) + bv64(0x100000000)))) ^ (Sym1 | (bv32(0x0001) << bcast((bcast(Sym2, bv32(0x0020)) & bv32(0x001F)),bv32(0x0020))))), bv32(0x0001)) ^ bv32(0x5DC376E0)) - bv32(0x0001)) ^bv32(0x12A41CD3)) + bv32(0x1E186A88)))), bv64(0x00000040)))"""
@@ -84,17 +156,31 @@ expr = """(bv64(0x66FFFFFFF93D8D48) + sign_extend(~(byte_swap(((((ror((Mem32(Add
 
 expr = "(bv64(0x66FFFFFFF93D8D48) + bv64(sign_extend(~(bv32(byte_swap(((((sspam_ror((bv32(Mem32(bv64(Addr64((bv64(Addr64((bv64(Mem64((((((((((((((((((((((bv64(Sym0) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) * bv64(0x00000001)) + bv64(0x00000090)))) + bv64(0x00000000)))) + bv64(0x100000000)))))) ^ (bv32(Sym1) | (bv32(0x0001) << (bv32(Sym2) & bv32(0x001F))))), bv32(0x0001)) ^ bv32(0x5DC376E0))  - bv32(0x0001)) ^ bv32(0x12A41CD3)) + bv32(0x1E186A88))))) , bv64(0x00000040))))"
 
+expr = "((((((((((((((((((((((bv64(Sym0) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) - bv64(0x00000008)) * bv64(0x00000001)) + bv64(0x00000090)))"
 
 
-
-a = ast.parse(expr)
-ComputeBvSize(128).visit(a)
-print ast.dump(a)
-print astunparse.unparse(a)
-print getattr(a.body[0].value, 'nbits')
-
-a = arithm_simpl.run(a, 64)
-print astunparse.unparse(a)
+#a = ast.parse(expr)
+#ComputeBvSize(128).visit(a)
+#print ast.dump(a)
+#print astunparse.unparse(a)
+#print getattr(a.body[0].value, 'bvsize')
+#
+#a = arithm_simpl.run(a, 64)
+#print astunparse.unparse(a)
 # print simplifier.simplify(astunparse.unparse(a))
 
 # print getattr(a.body[0].value.left.left, 'nbits')
+
+expr = "bv32(Sym0)*bv32(256) & bv8(0xFF)"
+print expr
+a = ast.parse(expr)
+ComputeBvSize(32).visit(a)
+DisplayBvSize().visit(a)
+print "-"*80
+ReduceBvSize().visit(a)
+DisplayBvSize().visit(a)
+# arithm_simpl.run(a, 32) 
+print ast.dump(a)
+print astunparse.unparse(a)
+print getattr(a.body[0].value.left.right, 'bvsize')
+
